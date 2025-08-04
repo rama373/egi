ac356ee9d1038d4516e7574ed00cc6e92424a478
rama373 <reginamustikaramaregina@gmail.com>
2025-08-04 16:55:40 +0700

    Add files via upload

---
+++ 20250804-164604.cpp
@@ -0,0 +1,10 @@
+    #include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "regina mustika rama!" << endl;
+   cout << "X IPA!" << endl;
+   cout << "cianjur 30/11/2009!" << endl;
+   }
+    
